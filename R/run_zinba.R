19756f5fa02aa968f515196352db9570c2173d34
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-08-10 22:04:05 +0000

    added run_zinba.R

---
+++ R/run_zinba.R
@@ -0,0 +1,27 @@
+run.zinba=function(seq, align, gdna, cnvarray){
+
+#buildwindowdata=function(seq, align, gdna, cnvarray)
+#getsigwindows=function(file,covnames,threshold=.01,output)
+#basecountimport=function(inputfile, coordfile, outputfile, chromosome='all')
+
+#bpProfiles=read.table(out,header=TRUE,sep="\t")
+#bpVector=bpProfiles[,6:(ncol(bpProfiles)-1)]
+#peakCoords=apply(bpVector,1,peakbound)
+#refPeaks=cbind(bpProfiles[,1:5],t(peakCoords))
+#out <- paste(coordFile,"_REFINE_COORDS.txt",sep="")
+#write.table(refPeaks,out,quote=F,sep="\t",row.names=F,col.names=F)
+
+
+#peakbound=function(x)
+
+
+
+#time.start <- Sys.time()
+#time.end <- Sys.time()
+#print(difftime(time.end,time.start))
+
+	Perl.Path <- file.path(.path.package("zimba"), "Perl")
+	Fn.Path <- file.path(Perl.Path, "runGetPeaks.pl")
+        CMD=paste("--seq", seq,"--align", align,"--gdna", gdna,"--cnvarray", cnvarray,sep=" ")
+	system(CMD)
+}
