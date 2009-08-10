87521bb00cf4e0caae6a48dad587f9818e59a846
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-08-10 18:31:58 +0000

    app_import

---
+++ src/coord.h
@@ -0,0 +1,26 @@
+#ifndef COORD_H_
+#define COORD_H_
+
+#include <iostream>
+#include <cstring>
+
+class coord {
+	
+	public:
+			coord();
+			~coord();
+			coord(char[],unsigned short int, unsigned long int, unsigned long int,char[]);
+			coord(const coord&);
+			bool operator <(coord) const;
+
+			char * ident;
+			unsigned short int chrom;//2
+			unsigned long int start;//8
+			unsigned long int end;//8
+			char * strand;
+	
+	private:
+			
+};
+
+#endif /*COORD_H_*/
