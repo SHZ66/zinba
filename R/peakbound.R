1096cdcd663271ef01cff182fef6a2de9eaec6fa
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-08-10 23:59:48 +0000

    made changes to peakbound() to take path to coordfile,and return a matrix of peakcoords (might want to check this to see if you get same error from before), also fixed typos in man pages

--- R/peakbound.R
+++ R/peakbound.R
@@ -1,35 +1,44 @@
-peakbound=function(x){ 
-    ##assuming 500 bp window and flanking 500 bp windows
-    ##TO DO: 
-    ##1)make general so can handle any window size
-    ##2)make to possibly handle multiple peaks
-    ##3)make so can look for max in flanks if which.max=border
-    ##############################
-    #left bound
-    #find max of center window
-    max=which.max(x[501:1000])+500
-    hold=matrix(0, 1,500)
-    for(bound in 500:50){
-        X=(max-bound):max
-        Y=x[(max(max-bound,1)):max]
-        B=sum((X-mean(X))*Y)/sum((X-mean(X))^2)
-        hold[bound]=(B^2)*sum((X-mean(X))^2)/sum((Y-mean(Y))^2)
-    }
+peakbound=function(out){ 
+    bpProfiles=read.table(out,header=TRUE,sep="\t")
+    bpVector=bpProfiles[i,6:(ncol(bpProfiles)-1)]
+    peakbound2=function(x){
+    	##assuming 500 bp window and flanking 500 bp windows
+	##TO DO: 
+    	##1)make general so can handle any window size
+    	##2)make to possibly handle multiple peaks
+    	##3)make so can look for max in flanks if which.max=border
+    	##############################
+    	##############################
+    	#left bound
+    	#find max of center window
+    	max=which.max(x[501:1000])+500
+    	hold=matrix(0, 1,500)
+    	for(bound in 500:50){
+        	X=(max-bound):max
+        	Y=x[(max(max-bound,1)):max]
+        	B=sum((X-mean(X))*Y)/sum((X-mean(X))^2)
+        	hold[bound]=(B^2)*sum((X-mean(X))^2)/sum((Y-mean(Y))^2)
+    	}
 
-    fit=which.max(hold)+1
-    peakstart=max-fit
-    ##############################
-    #right bound
-    hold=matrix(0, 1,500)
-    for(bound in 50:500){
-        X=max:(max+bound)
-        Y=x[(max:(max+bound))]
-        B=sum((X-mean(X))*Y)/sum((X-mean(X))^2)
-        hold[bound]=(B^2)*sum((X-mean(X))^2)/sum((Y-mean(Y))^2)
+    	fit=which.max(hold)+1
+    	peakstart=max-fit
+    	##############################
+    	#right bound
+    	hold=matrix(0, 1,500)
+    	for(bound in 50:500){
+        	X=max:(max+bound)
+        	Y=x[(max:(max+bound))]
+        	B=sum((X-mean(X))*Y)/sum((X-mean(X))^2)
+        	hold[bound]=(B^2)*sum((X-mean(X))^2)/sum((Y-mean(Y))^2)
+    	}
+    	fit=which.max(hold)
+    	peakend=max+fit
+    	##############################
+    	return(c(peakstart, peakend))
     }
-    fit=which.max(hold)
-    peakend=max+fit
-    ##############################
-    return(c(peakstart, peakend))
+    peakCoords=apply(bpVector,1,peakbound2)
+    refPeaks=cbind(bpProfiles[,1:5],peakCoords)
+    print(refPeaks)
+
 }
 
