52d29cb362e80104dbc10f3a75e4a2250697c5a3
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-08-10 20:41:00 +0000

    updated man files for current 4 functions, updated buildpeakwindows.R so that pasted command calls perl script properly, changed references in R functions to zinba instead of zinba so files should be install now as R CMD INSTALL zinba

--- R/buildwindowdata.R
+++ R/buildwindowdata.R
@@ -1,7 +1,7 @@
 buildwindowdata=function(seq, align, gdna, cnvarray){
 	#need to update perl file by removing environment for hg18 path, incorporate into perl command
-	Perl.Path <- file.path(.path.package("zimba"), "Perl")
+	Perl.Path <- file.path(.path.package("zinba"), "src")
 	Fn.Path <- file.path(Perl.Path, "buildPeakWindows_v3.pl")
-        CMD=paste("--seq", seq,"--align", align,"--gdna", gdna,"--cnvarray", cnvarray,sep=" ")
+        CMD=paste("Fn.Path","--seq", seq,"--align", align,"--gdna", gdna,"--cnvarray", cnvarray,sep=" ")
 	system(CMD)    
 }
