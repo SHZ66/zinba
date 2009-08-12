b6a8823356ecb96fed777ba74cc40a083238f837
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-08-12 03:41:44 +0000

    more debug buildwindows.pl

--- R/buildwindowdata.R
+++ R/buildwindowdata.R
@@ -2,6 +2,7 @@ buildwindowdata=function(seq, align=NULL, input=NULL, cnvarray=NULL,twoBit=NULL,
 	#need to update perl file by removing environment for hg18 path, incorporate into perl command
 	Perl.Path <- file.path(.path.package("zinba"), "src")
 	Fn.Path <- file.path(Perl.Path, "buildPeakWindows_v3.pl")
-        CMD=paste("Fn.Path","--seq", seq,"--align", align,"--input", input,"--cnvarray", cnvarray,"--twoBit",twoBit,"--window-size",winSize,"--offset-size",offset,"--align-thresh",aThresh,"--gb",gb,sep=" ")
+        CMD=paste("Fn.Path","--seq", seq,"--align", align,"--cnvarray", cnvarray,"--twoBit",twoBit,"--offset-size",offset,"--align-thresh",aThresh,"--gb",gb,sep=" ")
+#        CMD=paste("Fn.Path","--seq", seq,"--align", align,"--input", input,"--cnvarray", cnvarray,"--twoBit",twoBit,"--window-size",winSize,"--offset-size",offset,"--align-thresh",aThresh,"--gb",gb,sep=" ")
 	system(CMD)    
 }
