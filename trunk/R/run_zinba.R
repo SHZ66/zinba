4e07092b88ee686cdded54c04911955dc419a7b8
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-08-19 20:53:21 +0000

    update, run zinba not working

--- trunk/R/run_zinba.R
+++ trunk/R/run_zinba.R
@@ -1,8 +1,8 @@
 run.zinba=function(seq,align=NULL,input=NULL,cnvarray=NULL,twoBit=NULL,winSize=500,offset=0,aThresh=1,gb=NULL,basecountfile=NULL,covs=NULL,threshold=0.01){
 	time.start <- Sys.time()
-	buildwindowdata(seq=seq,align=align,cnvarray=cnvarray,twoBit=twoBit,offset=offset,aThresh=aThresh,gb=gb)    
-
+	buildwindowdata(seq=seq,align=align,input=input,cnvarray=cnvarray,twoBit=twoBit,winSize=winSize,offset=offset,aThresh=aThresh,gb=gb)
 	data_list <- read.table(paste(seq,".list",sep=""))
+
         for (i in 1:nrow(data_list)){
             coordout <- paste(substr(data_list[i,1],1,((nchar(as.character(data_list[i,1])))-4)),"_PEAK_COORDS.temp",sep="")
             winout <- paste(substr(data_list[i,1],1,((nchar(as.character(data_list[i,1])))-4)),".wins",sep="")
@@ -13,9 +13,9 @@ run.zinba=function(seq,align=NULL,input=NULL,cnvarray=NULL,twoBit=NULL,winSize=5
 	    basecountimport(inputfile=basecountfile,coordfile=coordout,outputfile=bpOut,chromosome=data_list[i,2])
 	    peakbound=function(profile=bpOut,output=peakout)
 
-#            unlink(coordout)
-#            unlink(winout)
-#            unlink(bpOut)
+            #unlink(coordout)
+            #unlink(winout)
+            #unlink(bpOut)
         }
 #        unlink(paste(seq,".list",sep=""))
 	time.end <- Sys.time()
