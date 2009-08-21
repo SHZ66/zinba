c6a52ea7765998aa0362fc77ce377de0b07bc3ef
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-08-21 15:52:31 +0000

    latest updates

--- R/buildwindowdata.R
+++ R/buildwindowdata.R
@@ -1,7 +1,6 @@
-buildwindowdata=function(seq, align=NULL, input=NULL, cnvarray=NULL,twoBit=NULL,winSize=500,offset=0,aThresh=1,gb=NULL){
+buildwindowdata=function(seq, align=NULL, input=NULL, cnvarray=NULL,twoBit=NULL,winSize=500,offset=0,aThresh=1,percN=0.1,gb=NULL){
 	Perl.Path <- file.path(.path.package("zinba"), "exec")
 	Fn.Path <- file.path(Perl.Path, "buildPeakWindows_v3.pl")
-        CMD=paste(Fn.Path,"--seq", seq,"--align", align,"--cnvarray", cnvarray,"--twoBit",twoBit,"--offset-size",offset,"--align-thresh",aThresh,"--gb",gb,sep=" ")
-#        CMD=paste(Fn.Path,"--seq", seq,"--align", align,"--input", input,"--cnvarray", cnvarray,"--twoBit",twoBit,"--window-size",winSize,"--offset-size",offset,"--align-thresh",aThresh,"--gb",gb,sep=" ")
+        CMD=paste(Fn.Path,"--seq", seq,"--align", align,"--cnvarray", cnvarray,"--twoBit",twoBit,"--win-size",winSize,"--offset-size",offset,"--align-thresh",aThresh,"--perc-n-thresh",percN,"--gb",gb,sep=" ")
 	system(CMD)    
 }
--- R/run_zinba.R
+++ R/run_zinba.R
@@ -1,7 +1,7 @@
-run.zinba=function(seq,align=NULL,input=NULL,cnvarray=NULL,twoBit=NULL,winSize=500,offset=0,aThresh=1,gb=NULL,basecountfile=NULL,covs=NULL,threshold=0.01){
+run.zinba=function(seq,align=NULL,input=NULL,cnvarray=NULL,twoBit=NULL,winSize=500,offset=0,aThresh=1,percN=0.1,gb=NULL,basecountfile=NULL,covs=NULL,threshold=0.01,method='pscl'){
 #need to add nThresh
 	time.start <- Sys.time()
-	buildwindowdata(seq=seq,align=align,input=input,cnvarray=cnvarray,twoBit=twoBit,winSize=winSize,offset=offset,aThresh=aThresh,gb=gb)
+	buildwindowdata(seq=seq,align=align,input=input,cnvarray=cnvarray,twoBit=twoBit,winSize=winSize,offset=offset,aThresh=aThresh,percN=percN,gb=gb)
 	data_list <- read.table(paste(seq,".list",sep=""))
 
         for (i in 1:nrow(data_list)){
@@ -10,7 +10,7 @@ run.zinba=function(seq,align=NULL,input=NULL,cnvarray=NULL,twoBit=NULL,winSize=5
             peakout <- paste(substr(data_list[i,1],1,((nchar(as.character(data_list[i,1])))-4)),".peaks",sep="")
             bpOut <- paste(coordout,"_BPcount",sep="")
 
-	    getsigwindows(file=as.character(data_list[i,1]),covnames=covs,threshold=threshold,winout=winout,coordout=coordout,offset=(winSize/2))
+	    getsigwindows(file=as.character(data_list[i,1]),covnames=covs,threshold=threshold,winout=winout,coordout=coordout,offset=(winSize/2),method=method)
 	    basecountimport(inputfile=basecountfile,coordfile=coordout,outputfile=bpOut,chromosome=data_list[i,2])
 	    peakbound(profile=bpOut,output=peakout)
 
