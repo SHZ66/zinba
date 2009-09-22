be10fcf87c034f3b7d46354b9c82d4a99ef33389
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-09-22 18:37:34 +0000

    modified cnv estimate to include new alignability score and custom cnv coordinates

--- R/estimatecnv.R
+++ R/estimatecnv.R
@@ -1,28 +1,19 @@
-estimatecnv=function(seq=NULL,winSize=25000,offset=0,twoBit=NULL,nProcess=1,percN=0.1){
-        library(MASS)
+estimatecnv=function(seq=NULL,winFile=NULL,winSize=25000,offset=0,gb="hg18",nProcess=1,alignDir=NULL){
         options(scipen=999)
 
 	Perl.Path <- file.path(.path.package("zinba"), "exec")
 	Fn.Path <- file.path(Perl.Path, "buildCNVWindows.pl")
 
-        CMD=paste(Fn.Path,"--seq", seq,"--window-size",winSize,"--offset-size",offset,"--perc-n-thresh",percN,"--processes",nProcess,sep=" ")
+        CMD=paste(Fn.Path,"--seq", seq,"--window-size",winSize,"--offset-size",offset,"--gb",gb,"--processes",nProcess,sep=" ")
 	if(is.null(seq)){
 		print(paste("Seq must be defined, currently:",seq,sep=" "))
 	}else{
-		if (!is.null(twoBit)){
-			CMD=paste(CMD,"--twoBit",twoBit,sep=" ")
+		if (!is.null(winFile)){
+			CMD=paste(CMD,"--window_file",winFile,sep=" ")
+		}
+		if (!is.null(alignDir)){
+			CMD=paste(CMD,"--align-dir",alignDir,sep=" ")
 		}
 		system(CMD)
 	}
-	#cnv_file <- paste(substr(seq,1,((nchar(as.character(seq)))-5)),".cnvs",sep="")
-	#
-	#cnv_data=read.table(cnv_file,header=F)
-	#winout <- paste(substr(seq,1,((nchar(as.character(seq)))-5)),"_RESIDUAL.cnvs",sep="")
-	#data=read.table(as.character(cnv_file),header=TRUE,sep="\t")
-	#colnames(data)=c('chromsome','start','stop','offset','exp_count')
-	#       
-	#a=glm.nb(data$exp_count ~ 1)        
-	#cnv_vals=cbind(data,residuals(a))
-	#colnames(cnv_vals)[dim(cnv_vals)[2]]=c('residuals')
-	#write.table(cnv_vals,winout,quote=F,sep="\t",row.names=F,col.names=T)
 }
