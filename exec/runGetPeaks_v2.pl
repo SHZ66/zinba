3379040f22f181c2d723b361a754e90c5fdd8e8b
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-10-05 21:44:20 +0000

    changes

--- exec/runGetPeaks_v2.pl
+++ exec/runGetPeaks_v2.pl
@@ -53,7 +53,7 @@ foreach my $chrm (keys %{$filesOffsets}){
     print STDERR "Processing $chrm\n";
     my $offsetFiles = join(";",@{$filesOffsets->{$chrm}});
     my $pid = $pm->start and next;
-    &run_zinba($offsetFiles,$coordout,$winout,$formula,$threshold,$winsize,$method,$stdlog,$errlog,$printLog);
+    &run_zinba($offsetFiles,$coordout,$winout,$formula,$threshold,$winsize,$method,$stdlog,$errlog,$printLog,$getRefinePeaks);
     $pm->finish;
 }
 $pm->wait_all_children;
@@ -64,12 +64,12 @@ if ($getRefinePeaks == 1){
     }else{
         system(qq`echo 'library(zinba);\nbasecountimport(inputfile="$bpCountFile",coordfile="$coordout",outputfile="$bpout");\npeakbound(bpprofile="$bpout",output="$peakout");\n' | R --vanilla --slave >> $stdlog 2>> $errlog`);
     }
-    unlink($bpout);
-    unlink($coordout);
+    #unlink($bpout);
+    #unlink($coordout);
 }
 
 sub run_zinba{
-    my ($inputFile,$coordout,$winout,$formula,$threshold,$winSize,$method,$stdLog,$errLog,$printLog) = @_;
+    my ($inputFile,$coordout,$winout,$formula,$threshold,$winSize,$method,$stdLog,$errLog,$printLog,$getRefinePeaks) = @_;
     my $off = $winSize/2;
     if ($printLog == 0){
         system(qq`echo 'library(zinba);\ngetsigwindows(file="$inputFile",formula=$formula,threshold=$threshold,winout="$winout",coordout="$coordout",offset=$off,getPeakRefine=$getRefinePeaks,method="$method");\n' | R --vanilla --slave > /dev/null 2> /dev/null`);
