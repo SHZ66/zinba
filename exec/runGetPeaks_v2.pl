07040c166fd0045e8579d9fd4ca557a261a60ac9
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-10-06 01:51:15 +0000

    add pass of winoffset for peakbound

--- exec/runGetPeaks_v2.pl
+++ exec/runGetPeaks_v2.pl
@@ -13,6 +13,7 @@ my $concurr_process = 1;
 my $winsize = 0;
 my $method = "pscl";
 my $printLog = 0;
+my $win_offset = undef;
 my $getRefinePeaks = 1;
 
 my $result = GetOptions(
@@ -23,11 +24,12 @@ my $result = GetOptions(
 	"method=s" => \$method,
 	"processes=i" => \$concurr_process,
         "refine_peaks=i" => \$getRefinePeaks,
+	"win-offset=s" => \$win_offset,
 	"print-log=i" => \$printLog
 );
-
 my $pm = new ForkManager_pg($concurr_process);
 
+$win_offset = $winsize/2 if(!defined($win_offset));
 my ($formula,$chrm,$data,$inputFile,$form,$offset,$out,$outFile);
 my ($coordout,$winout,$peakout,$stdlog,$errlog,$bpout,$filesOffsets);
 open(LIST,$paramFile);
@@ -53,28 +55,27 @@ foreach my $chrm (keys %{$filesOffsets}){
     print STDERR "Processing $chrm\n";
     my $offsetFiles = join(";",@{$filesOffsets->{$chrm}});
     my $pid = $pm->start and next;
-    &run_zinba($offsetFiles,$coordout,$winout,$formula,$threshold,$winsize,$method,$stdlog,$errlog,$printLog,$getRefinePeaks);
+    &run_zinba($offsetFiles,$coordout,$winout,$formula,$threshold,$winsize,$win_offset,$method,$stdlog,$errlog,$printLog,$getRefinePeaks);
     $pm->finish;
 }
 $pm->wait_all_children;
 
 if ($getRefinePeaks == 1){
     if ($printLog == 0){
-        system(qq`echo 'library(zinba);\nbasecountimport(inputfile="$bpCountFile",coordfile="$coordout",outputfile="$bpout");\npeakbound(bpprofile="$bpout",output="$peakout");\n' | R --vanilla --slave > /dev/null 2> /dev/null`);
+        system(qq`echo 'library(zinba);\nbasecountimport(inputfile="$bpCountFile",coordfile="$coordout",outputfile="$bpout");\npeakbound(bpprofile="$bpout",output="$peakout",winoffset=$win_offset);\n' | R --vanilla --slave > /dev/null 2> /dev/null`);
     }else{
-        system(qq`echo 'library(zinba);\nbasecountimport(inputfile="$bpCountFile",coordfile="$coordout",outputfile="$bpout");\npeakbound(bpprofile="$bpout",output="$peakout");\n' | R --vanilla --slave >> $stdlog 2>> $errlog`);
+        system(qq`echo 'library(zinba);\nbasecountimport(inputfile="$bpCountFile",coordfile="$coordout",outputfile="$bpout");\npeakbound(bpprofile="$bpout",output="$peakout",winoffset=$win_offset);\n' | R --vanilla --slave >> $stdlog 2>> $errlog`);
     }
     unlink($bpout);
     unlink($coordout);
 }
 
 sub run_zinba{
-    my ($inputFile,$coordout,$winout,$formula,$threshold,$winSize,$method,$stdLog,$errLog,$printLog,$getRefinePeaks) = @_;
-    my $off = $winSize/2;
+    my ($inputFile,$coordout,$winout,$formula,$threshold,$winSize,$winOffset,$method,$stdLog,$errLog,$printLog,$getRefinePeaks) = @_;
     if ($printLog == 0){
-        system(qq`echo 'library(zinba);\ngetsigwindows(file="$inputFile",formula=$formula,threshold=$threshold,winout="$winout",coordout="$coordout",offset=$off,getPeakRefine=$getRefinePeaks,method="$method");\n' | R --vanilla --slave > /dev/null 2> /dev/null`);
+        system(qq`echo 'library(zinba);\ngetsigwindows(file="$inputFile",formula=$formula,threshold=$threshold,winout="$winout",coordout="$coordout",offset=$winOffset,getPeakRefine=$getRefinePeaks,method="$method");\n' | R --vanilla --slave > /dev/null 2> /dev/null`);
     }else{
-        system(qq`echo 'library(zinba);\ngetsigwindows(file="$inputFile",formula=$formula,threshold=$threshold,winout="$winout",coordout="$coordout",offset=$off,getPeakRefine=$getRefinePeaks,method="$method");\n' | R --vanilla --slave >> $stdLog 2>> $errLog`);
+        system(qq`echo 'library(zinba);\ngetsigwindows(file="$inputFile",formula=$formula,threshold=$threshold,winout="$winout",coordout="$coordout",offset=$winOffset,getPeakRefine=$getRefinePeaks,method="$method");\n' | R --vanilla --slave >> $stdLog 2>> $errLog`);
     }
     return(0);
 }
