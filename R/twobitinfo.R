89cfee32e2a5ff362220ffbedc6b552212da9b9e
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-10-14 15:38:05 +0000

    update

--- R/twobitinfo.R
+++ R/twobitinfo.R
@@ -1,5 +1,4 @@
 twobitinfo=function(infile, outfile){
-        print(paste("infile is",infile,"outfile is",outfile,sep=" "))
 	.C("twoBitInfo",as.character(infile), as.character(outfile),PACKAGE="zinba")
 }
 	
--- R/twobittofa.R
+++ R/twobittofa.R
@@ -1,5 +1,4 @@
 twobittofa=function(chrm,start,end,twoBitFile,gcSeq){
-    print(paste("chrm start end twoBitFile gcSeq are",chrm,start,end,twoBitFile,gcSeq,sep=" "))
     .C("twoBitToFa",as.character(chrm),as.integer(start),as.integer(end),as.character(twoBitFile),as.character(gcSeq),PACKAGE="zinba")
 }
 	
