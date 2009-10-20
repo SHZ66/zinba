b3561c412ee3d497b1cacc6aaa4956d5bed2bab2
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-10-20 16:02:23 +0000

    added align adjust c++ code

---
+++ src/process_scores.h
@@ -0,0 +1,19 @@
+#ifndef PROCESS_SCORES_H_
+#define PROCESS_SCORES_H_
+
+#include <string>
+using namespace std;
+
+class process_scores{
+	
+	public:
+	
+		process_scores();
+		~process_scores();
+		int adjustCoords(const char *,const char *,int,int);
+
+	private:
+		
+};
+
+#endif
