b7e62871145515d1f9d2025c841c1f7d108bc5db
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-10-27 00:53:16 +0000

    standalone zeroinfl

--- trunk/src/peakboundc.c
+++ trunk/src/peakboundc.c
@@ -7,8 +7,9 @@
 #include <Rmath.h>
 
 #define min(a,b)  ((a) < (b) ? (a) : (b))
+#define maxx(x,y) ((x) > (y) ? (x) : (y))
 
-void peakboundc(int basecount[],int *Rlbasecount, int maxvec[], int *Rlmaxvec, int *Rsearchlength, int maxvecbounds[])
+void peakboundc(int basecount[],int *Rlbasecount, int maxvec[], int *Rlmaxvec, int *Rsearchlength, int results[])
 {
 
 
@@ -20,6 +21,7 @@ int  peakstart=0;
 int  peakend=0;
 int  fit=0;
 int  bound,h, i, j, n;
+int maxvecbounds[lmaxvec*2];
 double val,sumxy,sumx, sumx2, sumy, sumy2;
 
 for(j=0; j<lmaxvec; j++){
@@ -100,6 +102,55 @@ for(j=0; j<lmaxvec; j++){
 maxvecbounds[2*j]=peakstart;
 maxvecbounds[2*j+1]=peakend;
 }	
+
+int dist, l, mingap, indexmingap, sep;
+
+//final outputted vector of bounds after merging, contains zeros
+//intialize results vector
+results[0]=maxvecbounds[0];
+results[1]=maxvecbounds[1];
+
+h=0;
+i=1;
+while(i<lmaxvec){
+	//calculate metrics per round
+	dist=maxvec[i]-maxvec[i-1];
+	val=10000000;
+	fit=0;
+	for(l=maxvec[i-1]-1;l<maxvec[i]; l++){
+      		if(basecount[l] < val){
+       			val = basecount[l];
+			fit=l;
+		}
+	}
+	mingap=val;
+	indexmingap=fit;
+	sep=maxvecbounds[2*i]-results[2*h+1];
+
+	if(dist<=100){
+	//merge if too close
+		results[2*h]=min(results[2*h],maxvecbounds[2*i]);
+		results[2*h+1]=maxx(results[2*h+1],maxvecbounds[2*i+1]);		
+	}else if(dist>100 & mingap>1.0*basecount[maxvec[i-1]]/2 & sep<40){
+		//merge if not close enough but if bound overlap and no valley inbetween 
+		//need to normalize with respect to min of whole basecount?
+		results[2*h]=min(results[2*h],maxvecbounds[2*i]);
+		results[2*h+1]=maxx(results[2*h+1],maxvecbounds[2*i+1])	;
+	}else if(dist>100 & mingap<=1.0*basecount[maxvec[i-1]]/2 & sep<40){
+		//separate if not close enough with some bound overlap with valley inbetween 
+		//set new boundary in between at the site of the min in between
+		results[2*h+1]=indexmingap;
+		h=h+1;
+		results[2*h]=indexmingap +1 ;
+		results[2*h+1]=maxvecbounds[2*i+1];
+		}else if(dist>100 & sep>=40){
+		//separate peak here, no boundary overlap and too far 
+		h=h+1;
+		results[2*h]=maxvecbounds[2*i];
+		results[2*h+1]=maxvecbounds[2*i+1]	;
+	}
+	i=i+1;
+}
 
 
 }
