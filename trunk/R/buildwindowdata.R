6157989a2589e193848276dc7db7283d6e213bbb
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-10-27 20:01:42 +0000

    buildwindows updated R code

--- trunk/R/buildwindowdata.R
+++ trunk/R/buildwindowdata.R
@@ -1,41 +1,44 @@
-buildwindowdata=function(paramFile=NULL,seq=NULL, align=NULL, input=NULL, cnvarray=NULL,cnvexpwin=NULL,cnvexpcustom=NULL,rand=NULL,twoBit=NULL,winSize=500,offset=0,inputTrans=NULL,inputRandLog=NULL,percN=0.1,gb="hg18",nProcess=1){
-	Perl.Path <- file.path(.path.package("zinba"), "exec")
-	Fn.Path <- file.path(Perl.Path, "buildPeakWindows_v3.pl")
-        CMD=paste(Fn.Path,"--seq", seq,sep=" ")
-	if(is.null(seq)){
-		print(paste("Seq must be defined, currently:",seq,sep=" "))
-	}else{
-		if (!is.null(align)){
-			CMD=paste(CMD,"--align",align,sep=" ")
-		}
-		if (!is.null(input)){
-			CMD=paste(CMD,"--input",input,sep=" ")
-		}
-		if (!is.null(cnvarray)){
-			CMD=paste(CMD,"--cnvarray",cnvarray,sep=" ")
-		}
-		if (!is.null(cnvexpwin)){
-			CMD=paste(CMD,"--cnv-expwin",cnvexpwin,sep=" ")
-		}
-		if (!is.null(cnvexpcustom)){
-			CMD=paste(CMD,"--cnv-expcustom",cnvexpcustom,sep=" ")
-		}
-		if (!is.null(rand)){
-			CMD=paste(CMD,"--rand",rand,sep=" ")
-		}
-		if (!is.null(twoBit)){
-			CMD=paste(CMD,"--twoBit",twoBit,sep=" ")
-		}
-		if (!is.null(paramFile)){
-			CMD=paste(CMD,"--param-file",paramFile,sep=" ")
-		}
-		CMD=paste(CMD,"--window-size",winSize,"--offset-size",offset,"--perc-n-thresh",percN,"--gb",gb,"--processes",nProcess,sep=" ")
-		if (!is.null(inputTrans)){
-			CMD=paste(CMD,"--trans-input",sep=" ")
-		}
-		if (!is.null(inputRandLog)){
-			CMD=paste(CMD,"--input_rand_log2",sep=" ")
-		}
-		system(CMD)
-	}
+buildwindowdata=function(seq,input="none",align,twoBit,winSize=500,offset=0,cnvWinSize=100000,cnvOffset=0,paramFile){
+	cReturn <- .C("buildWindows",as.character(seq),as.character(input),as.character(align),as.character(twoBit),as.integer(winSize),as.integer(offset),as.integer(cnvWinSize),as.integer(cnvOffset),as.character(paramFile),PACKAGE="zinba")
 }
+#buildwindowdata=function(paramFile=NULL,seq=NULL, align=NULL, input=NULL, cnvarray=NULL,cnvexpwin=NULL,cnvexpcustom=NULL,rand=NULL,twoBit=NULL,winSize=500,offset=0,inputTrans=NULL,inputRandLog=NULL,percN=0.1,gb="hg18",nProcess=1){
+#	Perl.Path <- file.path(.path.package("zinba"), "exec")
+#	Fn.Path <- file.path(Perl.Path, "buildPeakWindows_v3.pl")
+#        CMD=paste(Fn.Path,"--seq", seq,sep=" ")
+#	if(is.null(seq)){
+#		print(paste("Seq must be defined, currently:",seq,sep=" "))
+#	}else{
+#		if (!is.null(align)){
+#			CMD=paste(CMD,"--align",align,sep=" ")
+#		}
+#		if (!is.null(input)){
+#			CMD=paste(CMD,"--input",input,sep=" ")
+#		}
+#		if (!is.null(cnvarray)){
+#			CMD=paste(CMD,"--cnvarray",cnvarray,sep=" ")
+#		}
+#		if (!is.null(cnvexpwin)){
+#			CMD=paste(CMD,"--cnv-expwin",cnvexpwin,sep=" ")
+#		}
+#		if (!is.null(cnvexpcustom)){
+#			CMD=paste(CMD,"--cnv-expcustom",cnvexpcustom,sep=" ")
+#		}
+#		if (!is.null(rand)){
+#			CMD=paste(CMD,"--rand",rand,sep=" ")
+#		}
+#		if (!is.null(twoBit)){
+#			CMD=paste(CMD,"--twoBit",twoBit,sep=" ")
+#		}
+#		if (!is.null(paramFile)){
+#			CMD=paste(CMD,"--param-file",paramFile,sep=" ")
+#		}
+#		CMD=paste(CMD,"--window-size",winSize,"--offset-size",offset,"--perc-n-thresh",percN,"--gb",gb,"--processes",nProcess,sep=" ")
+#		if (!is.null(inputTrans)){
+#			CMD=paste(CMD,"--trans-input",sep=" ")
+#		}
+#		if (!is.null(inputRandLog)){
+#			CMD=paste(CMD,"--input_rand_log2",sep=" ")
+#		}
+#		system(CMD)
+#	}
+#}
--- trunk/R/run_zinba.R
+++ trunk/R/run_zinba.R
@@ -1,11 +1,13 @@
-run.zinba=function(paramFile=NULL,seq=NULL,align=NULL,input=NULL,cnvarray=NULL,cnvexpwin=NULL,cnvexpcustom=NULL,rand=NULL,twoBit=NULL,winSize=500,offset=0,percN=0.1,inputTrans=NULL,inputRandLog=NULL,gb="hg18",basecountfile=NULL,threshold=0.01,numProc=1,buildwin=0,winoffset=0,refinepeaks=1,printLog=0,method='pscl'){
+#run.zinba=function(paramFile=NULL,seq=NULL,align=NULL,input=NULL,cnvarray=NULL,cnvexpwin=NULL,cnvexpcustom=NULL,rand=NULL,twoBit=NULL,winSize=500,offset=0,percN=0.1,inputTrans=NULL,inputRandLog=NULL,gb="hg18",basecountfile=NULL,threshold=0.01,numProc=1,buildwin=0,winoffset=0,refinepeaks=1,printLog=0,method='pscl'){
+run.zinba=function(paramFile=NULL,seq=NULL,align=NULL,input=NULL,twoBit=NULL,winSize=500,offset=0,cnvWinSize=100000,cnvOffset=0,percN=0.1,basecountfile=NULL,threshold=0.01,numProc=1,buildwin=0,winoffset=500,refinepeaks=1,printLog=0,method='pscl'){
 	time.start <- Sys.time()
         if(buildwin==1){
-            buildwindowdata(paramFile=paramFile,seq=seq,align=align,input=input,cnvarray=cnvarray,cnvexpwin=cnvexpwin,cnvexpcustom=cnvexpcustom,rand=rand,twoBit=twoBit,winSize=winSize,offset=offset,inputTrans=inputTrans,inputRandLog=inputRandLog,percN=percN,gb=gb,nProcess=numProc)
+#            buildwindowdata(paramFile=paramFile,seq=seq,align=align,input=input,cnvarray=cnvarray,cnvexpwin=cnvexpwin,cnvexpcustom=cnvexpcustom,rand=rand,twoBit=twoBit,winSize=winSize,offset=offset,inputTrans=inputTrans,inputRandLog=inputRandLog,percN=percN,gb=gb,nProcess=numProc)
+            buildwindowdata(paramFile=paramFile,seq=seq,align=align,input=input,twoBit=twoBit,winSize=winSize,offset=offset,cnvWinSize=cnvWinSize,cnvOffset=cnvOffset)
 	}
 	Perl.Path <- file.path(.path.package("zinba"), "exec")
 	Fn.Path <- file.path(Perl.Path, "runGetPeaks_v2.pl")
-	if(is.null(basecountfile)){
+	if(refinepeaks==1 && is.null(basecountfile)){
 		print(paste("Basecount file must be specified, currently",basecountfile,sep=" "))
 	}else if (is.null(paramFile)){
 		print(paste("Need parameter file with formula specified ",paramFile,sep=" "))
