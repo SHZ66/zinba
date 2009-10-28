0661b46c6e5bbc3fec083c2aa101f1541136fc96
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-10-28 21:19:48 +0000

    add basealigncount src

---
+++ src/baseAlignCounts.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include "bcanalysis.h"
+#include <ext/slist>
+#include <R.h>
+#include <Rmath.h>
+#define MAX_LEN 1025
+
+namespace sgi = ::__gnu_cxx;
+using namespace sgi;
+using namespace std;
+
+/*void print_ops(){
+		cout << "USAGE: baseAlignCounts inputFile.bed outputFile genomeBuild\n";
+		cout << "\tInput file must be formatted -> CHROM START STOP\n";
+		cout << "\t\tLines starting with track are ignored\n";
+}
+
+int main(int argc=0, char **argv=NULL){
+
+	if(argc < 3){
+		print_ops();
+		exit(1);
+	}else{
+		string inputFile = argv[1];
+		string outputFile = argv[2];
+		int extendLength = argv[3];
+*/
+extern "C"{
+void baseAlignCounts(char **RinputFile,char **RoutputFile, int *RextendLength){
+	string inputFile = RinputFile[0];
+	string outputFile = RoutputFile[0];
+	int extendLength = RextendLength[0];
+	
+		bc analysis newAnalysis;// = new analysis;
+		Rprintf("\nImporting reads from file %s ....\n",inputFile.c_str());
+		int ret=newAnalysis.importRawSignal(inputFile.c_str());
+
+		if(ret == 1){
+			Rprintf("ERROR opening file %s \n",expSeqFile.c_str());
+			exit(1);
+		}
+
+		Rprintf("Calculating counts at each base\nPrinting output to %s\n",outputFile.c_str());
+		ret = newAnalysis.processSignals(outputFile.c_str(),extendLength);
+		Rprintf("-------- BASE ALIGN COUNTS COMPLETE --------\n");
+	}
+	return 0;
+}
+}
