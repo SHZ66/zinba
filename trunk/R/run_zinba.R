a4db8d1f60660545f42d2749457e3481473d961d
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-10-30 20:02:32 +0000

    added peak win size and quantile

--- trunk/R/run_zinba.R
+++ trunk/R/run_zinba.R
@@ -1,5 +1,4 @@
-#run.zinba=function(paramFile=NULL,seq=NULL,align=NULL,input=NULL,cnvarray=NULL,cnvexpwin=NULL,cnvexpcustom=NULL,rand=NULL,twoBit=NULL,winSize=500,offset=0,percN=0.1,inputTrans=NULL,inputRandLog=NULL,gb="hg18",basecountfile=NULL,threshold=0.01,numProc=1,buildwin=0,winoffset=0,refinepeaks=1,printLog=0,method='pscl'){
-run.zinba=function(paramFile=NULL,seq=NULL,align=NULL,input=NULL,twoBit=NULL,winSize=500,offset=0,cnvWinSize=100000,cnvOffset=0,basecountfile=NULL,threshold=0.01,numProc=1,buildwin=0,winoffset=500,refinepeaks=1,printLog=0,method='pscl'){
+run.zinba=function(paramFile=NULL,seq=NULL,align=NULL,input=NULL,twoBit=NULL,winSize=500,offset=0,cnvWinSize=100000,cnvOffset=0,basecountfile=NULL,threshold=0.01,numProc=1,buildwin=0,pWinSize=200,pquant=0.75,refinepeaks=1,printLog=0,method='pscl'){
 	time.start <- Sys.time()
         if(buildwin==1){
             buildwindowdata(paramFile=paramFile,seq=seq,align=align,input=input,twoBit=twoBit,winSize=winSize,offset=offset,cnvWinSize=cnvWinSize,cnvOffset=cnvOffset)
@@ -14,7 +13,7 @@ run.zinba=function(paramFile=NULL,seq=NULL,align=NULL,input=NULL,twoBit=NULL,win
                 print(paste("Getting significant windows"))
                 print(paste("Using parameter file",paramFile,sep=" "))
                 print(paste("Running",numProc,"parallel jobs",sep=" "))
-	        CMD=paste(Fn.Path,"--param-file", paramFile,"--threshold",threshold,"--win-size",winSize,"--basecount_file",basecountfile,"--method",method,"--processes",numProc,"--refine_peaks",refinepeaks,"--win-offset",winoffset,sep=" ")
+	        CMD=paste(Fn.Path,"--param-file", paramFile,"--threshold",threshold,"--win-size",winSize,"--basecount_file",basecountfile,"--method",method,"--processes",numProc,"--refine_peaks",refinepeaks,"--peakwin-size",pWinSize,"--peak-quant",pquant,sep=" ")
 		if(printLog==1){
 			CMD=paste(CMD,"--print-log 1",sep=" ")
 		}
