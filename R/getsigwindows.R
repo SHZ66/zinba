db16754ed43f5a9ce72e7e6af3880f1315c523e9
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-11-07 16:47:07 +0000

    C for for GLM

--- R/getsigwindows.R
+++ R/getsigwindows.R
@@ -60,7 +60,7 @@ getsigwindows=function(file,formula,threshold=.01,peakconfidence=.8,priorpeakpro
 		q25=quantile(data$exp_count, 0.25)
                 mf <- model.frame(formula=formula, data=data)
                 X <- model.matrix(attr(mf, "terms"), data=mf)
-		
+		XNB=X[,-c(1)]
 		logsumexp=function(v){
 			if(any(is.infinite(v))){
 				stop("infinite value in v\n")
@@ -73,13 +73,13 @@ getsigwindows=function(file,formula,threshold=.01,peakconfidence=.8,priorpeakpro
 		}
           
 		loglikfun=function(parms){
-			mu1=exp(X%*%parms[1:kx])
-			mu2=exp(X%*%parms[(kx+1):(kx+kz)])
-			theta1 <- exp(parms[(kx+kz+kz)+1])
-		        theta2 <- exp(parms[(kx+kz+kz)+2])	
-			prob0 <- as.vector(linkinv(Z %*% parms[(kx+kz+1):(kx+kz+kz)]))
-			prop1=parms[kx+kz+kz+3]
-			prop2=parms[kx+kz+kz+4]
+			mu1=parms$start$count1
+			mu2=parms$start$count2
+			prob0 =parms$start$zero
+			prop1=parms$prop1
+			prop2=parms$prop2
+			theta1=parms$start$theta1
+			theta2=parms$start$theta2
 			loglik0=log(prob0+prop1*dnbinom(0, size = theta1, mu = mu1)+prop2*dnbinom(0, size = theta2, mu = mu2))
 			loglik1=log(prop1*dnbinom(Y, size = theta1, mu = mu1)+prop2*dnbinom(Y, size = theta2, mu = mu2))
 			NAs=which(loglik1==-Inf | loglik1==-Inf)
@@ -101,7 +101,8 @@ getsigwindows=function(file,formula,threshold=.01,peakconfidence=.8,priorpeakpro
                 linkobj <- make.link(linkstr)
                 linkinv <- linkobj$linkinv
 			#starting params for ze0 component
-			model_zero <- suppressWarnings(glm.fit(Z, as.integer(Y0),family = binomial(link = linkstr)))
+		model_zero <-.C("pglm_fit", family=as.integer(1), N=as.integer(length(Y)), M=as.integer(ncol(X)), y=as.double(Y0), prior=as.double(rep(1,n)), offset=as.double(rep(0,n)), X=as.double(unlist(X)),  stratum=as.integer(rep(1,n)),init=as.integer(0), rank=integer(1), Xb=double(n*ncol(X)), fitted=as.double((rep(1,n) * Y0 + 0.5)/(rep(1,n) + 1)), resid=double(n), weights=double(n),scale=double(1), df_resid=integer(1), theta=as.double(-1), package='zinba')  
+
 			prop0=sum( model_zero$fitted)/n
 	
 			#starting params for count componenets
@@ -111,21 +112,23 @@ getsigwindows=function(file,formula,threshold=.01,peakconfidence=.8,priorpeakpro
 			prop1=1-prop0-prop2
 			odY = order(Y)
 			n1  = round(length(Y) * (1 - prop2))
-			priorCOUNTweight=rep(1, length(Y))      
-			priorCOUNTweight[odY[1:n1]]=0
+			priorCOUNTweight=rep(1-10^-10, length(Y))      
+			priorCOUNTweight[odY[1:n1]]=10^-10
 	
-			model_count1 <- suppressWarnings(glm.fit(X, Y, family = poisson(), weights = (1-priorCOUNTweight)))
-			model_count2 <- suppressWarnings(glm.fit(X, Y, family = poisson(), weights = (priorCOUNTweight)))
-	
-			#start parameter vector
-			start <- list(count1 = model_count1$coefficients, count2 = model_count2$coefficients,zero = model_zero$coefficients)
-			start$theta1 <- 1
-			start$theta2 <- 1
-	
+			model_count1 <- .C("pglm_fit", family=as.integer(2), N=as.integer(length(Y)), M=as.integer(ncol(XNB)), y=as.double(Y), prior=as.double(1-priorCOUNTweight), offset=as.double(rep(0,length(Y))), X=as.double(unlist(XNB)),  stratum=as.integer(rep(1,length(Y))),init=as.integer(1), rank=integer(1), Xb=double(length(Y)*ncol(XNB)), fitted=as.double(Y+(Y==0)/6), resid=double(length(Y)), weights=double(length(Y)),scale=double(1), df_resid=integer(1), theta=as.double(-1), package='zinba')  
+
+			model_count2 <- .C("pglm_fit", family=as.integer(2), N=as.integer(length(Y)), M=as.integer(ncol(XNB)), y=as.double(Y), prior=as.double(priorCOUNTweight), offset=as.double(rep(0,length(Y))), X=as.double(unlist(XNB)),  stratum=as.integer(rep(1,length(Y))),init=as.integer(1), rank=integer(1), Xb=double(length(Y)*ncol(XNB)), fitted=as.double(Y+(Y==0)/6), resid=double(length(Y)), weights=double(length(Y)),scale=double(1), df_resid=integer(1), theta=as.double(-1), package='zinba')  
+
 			#starting prior probs
 			mui1  <- model_count1$fitted
 			mui2  <- model_count2$fitted
 			probi0 <- model_zero$fitted
+
+		        #start mean vector
+			start <- list(count1 = model_count1$fitted, count2 = model_count2$fitted,zero = model_zero$fitted, zerocoef=model_zero$coefficients)
+			start$theta1 <- 1
+			start$theta2 <- 1
+	
 	
 			probi0=probi0/(probi0+prop1*dnbinom(Y, size = start$theta1, mu = mui1)+ prop2*dnbinom(Y, size = start$theta2, mu = mui2))
 			probi0[Y1]=0
@@ -136,7 +139,7 @@ getsigwindows=function(file,formula,threshold=.01,peakconfidence=.8,priorpeakpro
 				probi1[NAs]=0
 				probi2[NAs]=1
 			}	
-		ll_new <- loglikfun(c(start$count1, start$count2, start$zero, log(start$theta1), log(start$theta2), prop1, prop2))
+		ll_new <- loglikfun(list(start=start, prop1=prop1, prop2=prop2))
 
 		ll_old <- 2 * ll_new      
 
@@ -154,10 +157,16 @@ getsigwindows=function(file,formula,threshold=.01,peakconfidence=.8,priorpeakpro
 			 prop2=sum(probi2)/n
 			 #updated values for parameters of component means
 		         
-			model_zero <- suppressWarnings(glm.fit(Z, probi0, family = binomial(link = linkstr), start = start$zero))
-			model_count1 <- glm.nb(Y ~ 0 + X, weights = (probi1),start = start$count1, init.theta = start$theta1)
-			model_count2 <- glm.nb(Y ~ 0 + X, weights = (probi2),start = start$count2, init.theta = start$theta2)
-			start <- list(count1 = model_count1$coefficients, count2 = model_count2$coefficients,theta1 = model_count1$theta, theta2 = model_count2$theta, zero = model_zero$coefficients)
+			model_zero <- .C("pglm_fit", family=as.integer(1), N=as.integer(length(Y)), M=as.integer(ncol(X)), y=as.double(probi0), prior=as.double(rep(1,n)), offset=as.double(rep(0,n)), X=as.double(unlist(X)),  stratum=as.integer(rep(1,n)),init=as.integer(0), rank=integer(1), Xb=double(n*ncol(X)), fitted=as.double((rep(1,n) * probi0 + 0.5)/(rep(1,n) + 1)), resid=double(n), weights=double(n),scale=double(1), df_resid=integer(1), theta=as.double(-1), package='zinba')  
+
+			model_count1 <- .C("pglm_fit", family=as.integer(0), N=as.integer(length(Y)), M=as.integer(ncol(XNB)), y=as.double(Y), prior=as.double(probi1), offset=as.double(rep(0,length(Y))), X=as.double(unlist(XNB)),  stratum=as.integer(rep(1,length(Y))),init=as.integer(1), rank=integer(1), Xb=double(length(Y)*ncol(XNB)), fitted=as.double(start$count1), resid=double(length(Y)), weights=double(length(Y)),scale=double(1), df_resid=integer(1), theta=as.double(start$theta1), package='zinba')  
+
+			model_count2 <- .C("pglm_fit", family=as.integer(0), N=as.integer(length(Y)), M=as.integer(ncol(XNB)), y=as.double(Y), prior=as.double(probi2), offset=as.double(rep(0,length(Y))), X=as.double(unlist(XNB)),  stratum=as.integer(rep(1,length(Y))),init=as.integer(1), rank=integer(1), Xb=double(length(Y)*ncol(XNB)), fitted=as.double(start$count2), resid=double(length(Y)), weights=double(length(Y)),scale=double(1), df_resid=integer(1), theta=as.double(start$theta2), package='zinba')  
+ 
+
+			start <- list(count1 = model_count1$fitted, count2 = model_count2$fitted,zero = model_zero$fitted, zerocoef=model_zero$coefficients)
+			start$theta1 <- model_count1$theta
+			start$theta2 <- model_count2$theta
 
 			mui1  <- model_count1$fitted
 			mui2  <- model_count2$fitted
@@ -173,7 +182,7 @@ getsigwindows=function(file,formula,threshold=.01,peakconfidence=.8,priorpeakpro
 				probi2[NAs]=1
 			}
 
-			ll_new <- loglikfun(c(start$count1, start$count2, start$zero, log(start$theta1), log(start$theta2), prop1, prop2))
+			ll_new <- loglikfun(list(start=start, prop1=prop1, prop2=prop2))
 
 			ll[i]=ll_new
 			i=i+1 
