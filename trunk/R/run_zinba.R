c2eda006dd88f8a9df191895ac6bb7680b586778
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-11-07 20:09:36 +0000

    parallel  procesing in R

--- trunk/R/run_zinba.R
+++ trunk/R/run_zinba.R
@@ -1,23 +1,43 @@
-run.zinba=function(paramFile=NULL,seq=NULL,align=NULL,input=NULL,twoBit=NULL,winSize=500,offset=0,cnvWinSize=100000,cnvOffset=0,basecountfile=NULL,threshold=0.01,numProc=1,buildwin=0,pWinSize=200,pquant=0.75,refinepeaks=1,printLog=0,method='pscl'){
+run.zinba=function(paramFile=NULL,formula=NULL,outfile=NULL,seq=NULL,align=NULL,input=NULL,twoBit=NULL,winSize=500,offset=0,cnvWinSize=100000,cnvOffset=0,basecountfile=NULL,threshold=0.01,peakconfidence=.8,priorpeakprop=.15,tol=10^-5,numProc=1,buildwin=0,pWinSize=200,pquant=0.75,refinepeaks=1,printLog=0,method='pscl'){
+        library(multicore)
+        library(doMC)
+        library(foreach)
 	time.start <- Sys.time()
         if(buildwin==1){
             buildwindowdata(paramFile=paramFile,seq=seq,align=align,input=input,twoBit=twoBit,winSize=winSize,offset=offset,cnvWinSize=cnvWinSize,cnvOffset=cnvOffset)
 	}
-	Perl.Path <- file.path(.path.package("zinba"), "exec")
-	Fn.Path <- file.path(Perl.Path, "runGetPeaks_v2.pl")
+#	Perl.Path <- file.path(.path.package("zinba"), "exec")
+#	Fn.Path <- file.path(Perl.Path, "runGetPeaks_v2.pl")
 	if(refinepeaks==1 && is.null(basecountfile)){
 		print(paste("Basecount file must be specified, currently",basecountfile,sep=" "))
 	}else if (is.null(paramFile)){
 		print(paste("Need parameter file with formula specified ",paramFile,sep=" "))
 	}else{
-                print(paste("Getting significant windows"))
-                print(paste("Using parameter file",paramFile,sep=" "))
-                print(paste("Running",numProc,"parallel jobs",sep=" "))
-	        CMD=paste(Fn.Path,"--param-file", paramFile,"--threshold",threshold,"--win-size",winSize,"--basecount_file",basecountfile,"--twobitFile",twoBit,"--method",method,"--processes",numProc,"--refine_peaks",refinepeaks,"--peakwin-size",pWinSize,"--peak-quant",pquant,sep=" ")
-		if(printLog==1){
-			CMD=paste(CMD,"--print-log 1",sep=" ")
-		}
-		system(CMD)
+            params=scan(paramFile,what=character(0))
+            winout=paste(outfile,".wins",sep="")
+            peakout=paste(outfile,".peaks",sep="")
+            coordout=paste(outfile,".coords",sep="")
+            bpout=paste(outfile,".bpcount",sep="")
+
+            registerDoMC(numProc)
+            mcoptions <- list(preschedule = FALSE, set.seed = FALSE)
+            getDoParWorkers()
+            rf <- foreach(i=1:length(params),.options.multicore = mcoptions) %dopar%
+                getsigwindows(file=params[i],formula=formula,threshold=threshold,winout=winout,coordout=coordout,getPeakRefine=refinepeaks,peakconfidence=peakconfidence,priorpeakprop=priorpeakprop,tol=tol,method=method)
+            rf
+            #basecountimport(inputfile=basecountfile,coordfile=coordout,outputfile=bpout,twobitfile=twoBit)
+            #peakbound(bpprofile=bpout,output=peakout,winSize=pWinSize,quantile=pquant)
+            #unlink(coordout)
+            #unlink(bpout)
+
+#                print(paste("Getting significant windows"))
+#                print(paste("Using parameter file",paramFile,sep=" "))
+#                print(paste("Running",numProc,"parallel jobs",sep=" "))
+#	        CMD=paste(Fn.Path,"--param-file", paramFile,"--threshold",threshold,"--win-size",winSize,"--basecount_file",basecountfile,"--twobitFile",twoBit,"--method",method,"--processes",numProc,"--refine_peaks",refinepeaks,"--peakwin-size",pWinSize,"--peak-quant",pquant,sep=" ")
+#		if(printLog==1){
+#			CMD=paste(CMD,"--print-log 1",sep=" ")
+#		}
+#		system(CMD)
 	}
 	time.end <- Sys.time()
 	print(difftime(time.end,time.start))
