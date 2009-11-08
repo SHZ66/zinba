045658ddbb79fc99008c56fba289c082cd55596d
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-11-08 01:43:25 +0000

    update buildwindows to output list

--- trunk/R/getrefinedpeaks.R
+++ trunk/R/getrefinedpeaks.R
@@ -1,4 +1,4 @@
-getrefinedpeaks=function(winout,coordout,basecountfile,bpout,twoBit,pWinSize,pquant,threshold=.01,method='pscl'){
+getrefinedpeaks=function(winout,coordout,basecountfile,bpout,peakout,twoBit,pWinSize,pquant,threshold=.01,method='pscl'){
     createcoordfile(file=winout,threshold=threshold,coordout=coordout,method=method)
     basecountimport(inputfile=basecountfile,coordfile=coordout,outputfile=bpout,twobitfile=twoBit)
     peakbound(bpprofile=bpout,output=peakout,winSize=pWinSize,quantile=pquant)
