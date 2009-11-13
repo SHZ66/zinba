e1c8ff5d577db5b1d58f36ee07262fddc6675737
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-11-13 18:52:08 +0000

    update

--- trunk/R/getrefinedpeaks.R
+++ trunk/R/getrefinedpeaks.R
@@ -1,4 +1,4 @@
-getrefinedpeaks=function(winlist,coordout,basecountfile,bpout,peakout,twoBit,pWinSize,pquant=0.75,peakconfidence=.8,threshold=.01,method='pscl'){
+getrefinedpeaks=function(winlist,coordout,basecountfile,bpout,peakout,twoBit,pWinSize=200,pquant=0.75,peakconfidence=.8,threshold=.01,method='pscl'){
     createcoordfile(filelist=winlist,threshold=threshold,peakconfidence=peakconfidence,coordout=coordout,method=method)
     basecountimport(inputfile=basecountfile,coordfile=coordout,outputfile=bpout,twobitfile=twoBit)
     peakbound(bpprofile=bpout,output=peakout,winSize=pWinSize,quantile=pquant)
