59b01fa55b622073ec4eb7b0701ef6b8c25f8aef
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-12-21 21:42:39 +0000

    update

--- trunk/R/buildwindowdata.R
+++ trunk/R/buildwindowdata.R
@@ -1,4 +1,4 @@
-buildwindowdata=function(seq,input="none",align,twoBit,winSize=500,offset=0,cnvWinSize=100000,cnvOffset=0,filelist){
-	cReturn <- .C("buildWindows",as.character(seq),as.character(input),as.character(align),as.character(twoBit),as.integer(winSize),as.integer(offset),as.integer(cnvWinSize),as.integer(cnvOffset),as.character(filelist),PACKAGE="zinba")
+buildwindowdata=function(seq,input="none",align,twoBit,winSize=500,offset=0,cnvWinSize=100000,cnvOffset=0,filelist,filetype="bowtie",extension){
+	cReturn <- .C("buildWindows",as.character(seq),as.character(input),as.character(align),as.character(twoBit),as.integer(winSize),as.integer(offset),as.integer(cnvWinSize),as.integer(cnvOffset),as.character(filetype),as.character(filelist),as.integer(extension),PACKAGE="zinba")
 	gc()
 }
