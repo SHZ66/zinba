1e6f3f889efd9202a9ab18e20896da7d84ea080f
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-12-30 20:15:20 +0000

    add peakseq mappability tools to exec dir

--- trunk/src/baseAlignCounts.cc
+++ trunk/src/baseAlignCounts.cc
@@ -21,19 +21,15 @@ void baseAlignCounts(char **RinputFile,char **RoutputFile, char **Rtwobitfile,in
 	int extendLength = RextendLength[0];
 	const char * filetype = Rfiletype[0];
 	
-		bcanalysis newAnalysis;// = new analysis;
-		Rprintf("\nImporting reads from file %s ....\n",inputFile);
-		Rprintf("Reads are formatted as %s ....\n",filetype);
-		int ret=newAnalysis.importRawSignal(inputFile,filetype);
-
-		if(ret == 1){
-			Rprintf("ERROR opening file %s \n",inputFile);
-			exit(1);
-		}
-
+	bcanalysis newAnalysis;// = new analysis;
+	Rprintf("\nImporting reads from file %s ....\n",inputFile);
+	Rprintf("Reads are formatted as %s ....\n",filetype);
+	int ret=newAnalysis.importRawSignal(inputFile,filetype);
+	if(ret == 0){
 		Rprintf("Calculating counts at each base\nPrinting output to %s\n",outputFile);
 		ret = newAnalysis.processSignals(outputFile,twobitfile,extendLength);
-		Rprintf("-------- BASE ALIGN COUNTS COMPLETE --------\n");
+	}
+	Rprintf("-------- BASE ALIGN COUNTS COMPLETE --------\n");
 
 }
 }
