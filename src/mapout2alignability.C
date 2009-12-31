306bb890165183d3ea978c8e0990101352bdc2fe
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2009-12-31 05:13:47 +0000

    add convert mappability

---
+++ src/mapout2alignability.C
@@ -0,0 +1,55 @@
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#define MAX_LEN 1025
+
+extern "C" {
+#include "R.h"
+#include "Rmath.h"
+#include "Rinternals.h"
+#include "Rdefines.h"
+}
+
+using namespace std;
+
+extern "C" {
+void mapout2alignability(char **Rfilelist,char **Routfile){
+	const char * filelist = Rfilelist[0];
+	const char * outfile = Routfile[0];
+
+	FILE * fout;
+	fout = fopen(outfile,"w"); 
+	fprintf(fout,"track type=wiggle_0 name=\"%s\" desc=\"%s\" visibility=full\n",filelist,filelist);
+
+	FILE * flist;
+	flist = fopen(filelist,"r");
+	char mapfile[128];
+	while(!feof(flist)){
+		fscanf(flist,"%s",mapfile);
+		Rprintf("\nProcessing %s .......\n",mapfile);
+		string chrm = string(mapfile);
+		size_t bout = chrm.find("b.out");
+		chrm.erase(bout);
+		Rprintf("\t%s .......\n",chrm.c_str());
+		fprintf(fout,"fixedStep chrom=%s start=1 step=1\n",chrm.c_str());
+		char mapChar[1];
+		int mval;
+		int countvals = 0;
+		ifstream mfile (mapfile,ios::in|ios::binary);
+		if(mfile.is_open()){
+			while(!mfile.eof()){
+				mfile.seekg(countvals,ios::beg);
+				mfile.read(mapChar,1);
+				unsigned char mchr = mapChar[0];
+				mval = (int) mchr;
+				fprintf(fout,"%d\n",mval);
+				countvals++;
+			}
+		}
+		mfile.close();
+	}
+	fclose(flist);
+}
+}
