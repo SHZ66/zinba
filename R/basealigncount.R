c9523a6fcb65acd86a51e08dc54bffd45d4725b8
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2010-01-18 00:48:55 +0000

    update

--- R/basealigncount.R
+++ R/basealigncount.R
@@ -1,3 +1,15 @@
-basealigncount=function(inputfile,outputfile,twoBitFile,extension,filetype="bowtie"){
+basealigncount=function(inputfile,outputfile,twoBitFile,extension=NULL,filetype="bowtie"){
+    if(!file.exists(inputfile)){
+    	stop(paste("Input file not found,",inputfile,sep=" "))
+    }
+    if(!file.exists(twoBit)){
+        stop(paste("twoBit file not found,",twoBit,sep=" "))
+    }
+    if(filetype != "bowtie" && filetype != "tagAlign" && filetype != "bed"){
+        stop(paste("Incorrect filetype:",filetype,"[bowtie|tagAlign|bed]",sep=" "))
+    }
+    if(is.null(extension)){
+        stop(paste("Need to specify an extension length",,sep=" "))
+    }
     cReturn <- .C("baseAlignCounts",as.character(inputfile), as.character(outputfile), as.character(twoBitFile),as.integer(extension),as.character(filetype),PACKAGE="zinba")
 }
