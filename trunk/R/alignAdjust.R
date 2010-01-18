4af3e6ab617b68117a75c0aa5b7d895b5124cae3
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2010-01-18 00:48:55 +0000

    update

--- trunk/R/alignAdjust.R
+++ trunk/R/alignAdjust.R
@@ -1,4 +1,10 @@
-alignAdjust=function(inputfile, outDir="",twoBitFile,athresh, adjustsize){
+alignAdjust=function(inputfile, outDir="",twoBitFile,athresh=1,adjustsize=0){
+    if(!file.exists(inputfile)){
+	stop(paste("Input file not found,",inputfile,sep=" "))
+    }
+    if(!file.exists(twoBit)){
+	stop(paste("twoBit file not found,",twoBit,sep=" "))
+    }
     .C("alignAdjust",as.character(inputfile),as.character(outDir),as.character(twoBitFile),as.integer(athresh),as.integer(adjustsize),PACKAGE="zinba")
     gc()
 }
