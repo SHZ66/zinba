4e5d5a16679726cf7ade0e346148ca8630e57d72
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2010-01-26 18:46:51 +0000

    update

--- src/calcCovs.h
+++ src/calcCovs.h
@@ -61,7 +61,8 @@ class calcCovs{
 		map<const char*, int, ltstr> chroms;//Implemented
 		map<int, const char*> intsToChrom;//Implemented
 	
-		map<unsigned short int,unsigned long int> chr_size;
+//		map<unsigned short int,unsigned long int> chr_size;
+		map<unsigned short int,unsigned int> chr_size;
 };
 
 #endif /*CALCCOVS_H_*/
