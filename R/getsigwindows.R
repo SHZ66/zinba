dc9be343174d60c16692a44a4081288a5fe56a42
lieblab <lieblab@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2010-06-14 00:49:37 +0000

    moved qvalue to global in getsigwindows

--- R/getsigwindows.R
+++ R/getsigwindows.R
@@ -1,6 +1,7 @@
 getsigwindows=function(file,formula,formulaE,formulaZ,threshold=.01,peakconfidence=.8,winout,printFullOut=0,tol=10^-5,method='pscl',initmethod, diff=0){
     time.start <- Sys.time()
     suppressPackageStartupMessages(library(quantreg))
+    suppressPackageStartupMessages(library(qvalue))
     library(MASS)
     options(scipen=999)
 
@@ -11,7 +12,7 @@ getsigwindows=function(file,formula,formulaE,formulaZ,threshold=.01,peakconfiden
     winfile=NULL
     printflag = 0
     if(method=='pscl'){
-        suppressPackageStartupMessages(library(qvalue))
+#        suppressPackageStartupMessages(library(qvalue))
         files = unlist(strsplit(file,";"))
         for(i in 1:length(files)){
             data=read.table(files[i], header=TRUE)
