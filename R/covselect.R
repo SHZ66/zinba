747148088b6e2e51154c1f5ed55ff48e08e0d73a
homeriq5 <homeriq5@69e3f3b7-d12e-3642-af49-08d3d55e0174>
2012-01-31 01:46:30 +0000

    small fix to covfile.R

--- R/covselect.R
+++ R/covselect.R
@@ -218,8 +218,8 @@ if(selection=="complete"){
 	#unequal number of columns?
 	if(length(table(fields)) != 1){
 		cat("Warning: intermediate (dirty) model file", loc, "has", length(skip), "lines with less than expected columns.  Removing problematic lines")
-		skip=which(fields != max(fields))
 	}
+	skip=which(fields != max(fields))
 
 	#no lines written to output?
 	if(lines == 0) cat("Intermediate (dirty) model file", loc, "has 0 lines written to it.  This suggests anomaly in model selection, please contact the package administrator")
